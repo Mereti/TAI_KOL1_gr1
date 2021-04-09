715d456d5b8c4f659b299ec7027468cfb0ffb436
Kinga <kinga2233@onet.pl>
2021-04-09 17:20:08 +0200

    kolokwium

---
+++ navbar.js
@@ -0,0 +1,4 @@
+let nav = '<nav><ul> <li><a href="index.html">Index  </a></li>' +
+    '<li><a href="kontakt.html">Kontakt</a></li>' +  '<li><a href="index.html">Index</a></li></ul</nav>';
+let navbar = document.querySelector(".nav-bar");
+navbar.innerHTML = nav;
---
+++ script.js
@@ -0,0 +1,6 @@
+
+const button =  document.querySelector(".button");
+const tlo =  document.querySelector(".tlo");
+
+button.onclick = function(){document.querySelector(".box").style.backgroundColor = "yellow";}
+tlo.onclick = function(){document.querySelector(".body").style.backgroundColor = "red";}
